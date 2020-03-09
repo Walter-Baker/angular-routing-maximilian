f3cc2033a2acc773f114c658bc6628b19811e0a3
Walter-Baker <waltercookiebaker@gmail.com>
2020-03-09 12:56:40 -0400

    1 file modified

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Observable } from 'rxjs'; // Not sure if needed.
 import { ServersService } from '../servers.service';
 
+import { CanComponentDeactivate } from './can-deactivate-guard.service';
+
 @Component({
     selector: 'app-edit-server',
     templateUrl: './edit-server.component.html'
@@ -10,6 +12,7 @@ import { ServersService } from '../servers.service';
 
 export class EditServerComponent implements OnInit {
     server: {id: number, name: string, status: string};
+    deltaServer: {id: number, name: string, status: string};
 
     constructor(private serversService: ServersService,
                 private route: ActivatedRoute,
@@ -17,7 +20,14 @@ export class EditServerComponent implements OnInit {
     }   
 
     ngOnInit(){
-        
+      const id = +this.route.snapshot.params['id'];
+      this.server = this.serversService.getServer(id);
+
+      this.deltaServer = {
+          id: this.server.id,
+          name: this.server.name,
+          status: this.server.status
+      }      
     }
 
     onUpdate(){
