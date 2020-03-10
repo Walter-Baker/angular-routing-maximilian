2a0699cf63640575549f90e4243c49b93c7bc912
Walter-Baker <waltercookiebaker@gmail.com>
2020-03-10 09:52:28 -0400

    1 file modified

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -12,7 +12,10 @@ import { CanComponentDeactivate } from './can-deactivate-guard.service';
 
 export class EditServerComponent implements OnInit {
     server: {id: number, name: string, status: string};
-    deltaServer: {id: number, name: string, status: string};
+    originalServer: {id: number, name: string, status: string};
+
+    changesSaved : boolean = false;
+    allowEdit: boolean = false;
 
     constructor(private serversService: ServersService,
                 private route: ActivatedRoute,
@@ -23,14 +26,40 @@ export class EditServerComponent implements OnInit {
       const id = +this.route.snapshot.params['id'];
       this.server = this.serversService.getServer(id);
 
-      this.deltaServer = {
+      this.originalServer = Object.assign(this.originalServer, this.server);
+
+      /*
+      this.originalServer = {
           id: this.server.id,
           name: this.server.name,
           status: this.server.status
-      }      
+      }  
+      */
+
+      this.route.queryParams
+        .subscribe(
+          (queryParams: Params) => {
+            this.allowEdit = queryParams['allowEdit'] === '1' ? true : false;
+          }
+        );
+      this.route.fragment.subscribe();
     }
 
-    onUpdate(){
-
+    onUpdate() {
+        this.serversService.updateServer(this.server.id, {name: this.server.name, status: this.server.status});
+        this.changesSaved = true;
+        this.router.navigate(['../'], {relativeTo: this.route});
     }
+
+    canDeactivate(): Observable<boolean> | Promise<boolean> | boolean {
+        if (!this.allowEdit) {
+          return true;
+        }
+        if ((this.originalServer.name !== this.server.name || this.originalServer.status !== this.server.status) && !this.changesSaved) {
+          return confirm('Do you want to discard the changes?');
+        } else {
+          return true;
+        }
+    }
+
 }
