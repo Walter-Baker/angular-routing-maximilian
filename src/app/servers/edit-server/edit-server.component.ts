9409a5090e7080bf6434575ce9138ec4d3492eee
Walter-Baker <waltercookiebaker@gmail.com>
2020-03-16 15:59:29 -0400

    2 files modified

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -10,6 +10,11 @@ import { ServersService } from '../servers.service';
   styleUrls: ['./edit-server.component.css']
 })
 export class EditServerComponent implements OnInit {
+  server: {id: number, name: string, status: string};
+  originalServer: {id: number, name: string, status: string};
+
+  changesSaved : boolean = false;
+  allowEdit: boolean = false;
 
   constructor(private serversService: ServersService,
               private route: ActivatedRoute,
@@ -17,7 +22,29 @@ export class EditServerComponent implements OnInit {
   }
 
   ngOnInit() {
+    const id = +this.route.snapshot.params['id']; // Selects the current id.
+    this.server = this.serversService.getServer(id); 
 
+    //this.originalServer = Object.assign(this.originalServer, this.server);
+
+    this.originalServer = {
+        id: this.server.id,
+        name: this.server.name,
+        status: this.server.status
+    }
+
+    this.route.queryParams
+      .subscribe(
+        ( queryParams : Params ) => {
+            this.allowEdit = queryParams['allowEdit'] === '1' ? true : false;
+        }
+      );
   }
+
+  onUpdate() {
+      this.serversService.updateServer(this.server.id, {name: this.originalServer.name, status: this.originalServer.status});
+      this.changesSaved = true;
+      this.router.navigate(['../'], {relativeTo: this.route});
+  }  
 
 }
