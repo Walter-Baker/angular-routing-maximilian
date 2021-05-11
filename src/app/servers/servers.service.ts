3007619921a1b606e8529f71b47d0a3589c804e1
Walter-Baker <waltercookiebaker@gmail.com>
2021-05-11 11:50:04 -0400

    1 file modified

--- src/app/servers/servers.service.ts
+++ src/app/servers/servers.service.ts
@@ -12,6 +12,12 @@ function GetById(group: IId[], id: number) {
     return item;
 }
 
+function UpdateItemFromValues(obj: any, values: any){
+    for (const [key, value] of Object.entries(values)) {
+        obj[key] = value;
+    }
+}
+
 export class ServersService {
   private servers = [
     {
@@ -50,6 +56,7 @@ export class ServersService {
     return GetById(this.servers, id)
   }
 
+/*
   updateServer(id: number, serverInfo: {name: string, status: string}) {
     const server = this.servers.find(
       (s) => {
@@ -61,4 +68,12 @@ export class ServersService {
       server.status = serverInfo.status;
     }
   }
+*/
+
+  updateServer(id: number, serverInfo: {name: string, status: string}) {
+    const server = GetById(this.servers, id);
+    UpdateItemFromValues(server, serverInfo);
+  }
+
+
 }
